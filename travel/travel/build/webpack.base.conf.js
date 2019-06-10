9710e15c8176995f734a3e63f9a7f3c61330123a
wgxg777 <1506884346@qq.com>
2019-06-10 16:53:53 +0800

    e

--- travel/travel/build/webpack.base.conf.js
+++ travel/travel/build/webpack.base.conf.js
@@ -91,3 +91,5 @@ module.exports = {
     child_process: 'empty'
   }
 }
+
+
--- travel/travel/src/pages/home/components/Swiper.vue
+++ travel/travel/src/pages/home/components/Swiper.vue
@@ -39,7 +39,7 @@ export default {
     width 100%
     height 0
     overflow hidden
-    padding-bottom 31.25%
+    padding-bottom 26.67%
     .swiper-img
         width 100%
 </style>
