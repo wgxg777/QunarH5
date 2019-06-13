393a13e99d1ef1dd312da66f9920a5cf772e7260
wgxg777 <1506884346@qq.com>
2019-06-13 16:45:59 +0800

    E

--- travel/travel/src/pages/home/components/Header.vue
+++ travel/travel/src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
         <span class="iconfont">&#xe632;</span>
         输入城市/游玩/主题
         </div>
-    <div class="header-right">
-       {{this.city}}
-       <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to='/city'>
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -30,7 +32,7 @@ export default {
     display: flex
     background-color $bgColor
     color:#fff
-    line-height: .86rem
+    line-height: $headerHeight
     .header-left
       width: .64rem
       float: left
@@ -52,6 +54,7 @@ export default {
       width: 1.24rem
       float: right
       text-align:center
+      color #fff
      .arrow-icon
        margin-left -.04rem
        font-size .2rem
